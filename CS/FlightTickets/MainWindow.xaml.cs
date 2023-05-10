9e0c8439297170bc91e4535b3d154815393181eb
Anzhelika Andronova (DevExpress) <anzhelika.andronova@devexpress.com>
2023-05-10 18:25:19 +0400

    Adding the example

---
+++ CS/FlightTickets/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using DevExpress.Mvvm.Native;
+using DevExpress.Mvvm;
+using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightTickets {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            ApplicationThemeHelper.ApplicationThemeName = "Office2019Colorful";
+            InitializeComponent();
+        }
+    }
+    class ViewModel : BindableBase {
+        readonly List<Flight> allFligths;
+        public ViewModel() {
+            allFligths = Enumerable.Range(0, 20)
+                .Select(_ => DataSource.NextFlight())
+                .ToList();
+
+            Min = DateTime.Today.AddDays(-1);
+            SearchCommand = new DelegateCommand(Search);
+            Search();
+        }
+
+        public DateTime Min {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+
+        public DateTime? Start {
+            get { return GetValue<DateTime?>(); }
+            set { SetValue(value); }
+        }
+
+        public DateTime? End {
+            get { return GetValue<DateTime?>(); }
+            set { SetValue(value); }
+        }
+
+        public List<string> Cities { get; } = DataSource.Cities;
+
+        public string From {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        public string To {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        public ObservableCollection<Flight> Flights { get; } = new ObservableCollection<Flight>();
+
+        public ICommand SearchCommand { get; }
+
+        void Search() {
+            Flights.Clear();
+            allFligths.Where(f => string.IsNullOrEmpty(From) || f.FromCity == From)
+                      .Where(f => string.IsNullOrEmpty(To) || f.ToCity == To)
+                      .Where(f => !Start.HasValue || f.Start >= Start)
+                      .Where(f => !End.HasValue || f.End <= End)
+                      .ForEach(Flights.Add);
+        }
+    }
+
+    class Flight {
+        public string FromCity { get; set; }
+        public string ToCity { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+    static class DataSource {
+        static readonly Random random = new Random();
+        public static List<string> Cities { get; } = new List<string> {
+            "Moscow", "Yerevan", "Beijing"
+        };
+        public static string NextCity() {
+            return Cities[random.Next(Cities.Count)];
+        }
+        public static DateTime NextDate() {
+            var offset = random.Next(-30, 30);
+            return DateTime.Today.AddDays(offset);
+        }
+        public static Flight NextFlight() {
+            var flight = new Flight {
+                FromCity = NextCity(),
+                ToCity = NextCity(),
+                Start = NextDate()
+            };
+            flight.End = flight.Start.AddDays(random.Next(14));
+            return flight;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,31 @@
+<!-- default badges list -->
+![](https://img.shields.io/endpoint?url=https://codecentral.devexpress.com/api/v1/VersionRange/128655103/23.1.2%2B)
+[![](https://img.shields.io/badge/Open_in_DevExpress_Support_Center-FF7200?style=flat-square&logo=DevExpress&logoColor=white)](https://supportcenter.devexpress.com/ticket/details/E4855)
+[![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
+<!-- default badges end -->
+
+# WPF DateRangeControl — Create a Control
+
+This example demonstrates how to create and use a [DateRangeControl](https://docs.devexpress.com/WPF/DevExpress.Xpf.Editors.DateRangeControl) in an application that searches flights for the tickets purchase.
+
+![Use a DateRangeControl](images/daterangecontrol.gif)
+
+## Implementation Details
+
+* Use DateRangeControl in XAML markup.
+* To customize the top area of the popup, use [DateRangeControl.PopupTopAreaTemplate](https://docs.devexpress.com/WPF/DevExpress.Xpf.Editors.DateRangeControl.PopupTopAreaTemplate).
+* To customize the bottom area of the popup, use [DateRangeControl.PopupBottomAreaTemplate](https://docs.devexpress.com/WPF/DevExpress.Xpf.Editors.DateRangeControl.PopupBottomAreaTemplate).
+* You can set predefined ranges in the [DateRangeControl.PredefinedRanges](https://docs.devexpress.com/WPF/DevExpress.Xpf.Editors.DateRangeControl.PredefinedRanges) property.
+
+## Documentation
+
+* [DateRangeControl](https://docs.devexpress.com/WPF/DevExpress.Xpf.Editors.DateRangeControl)
+* 
+
+<!-- default file list -->
+## Files to Review
+
+* [MainWindow.xaml](./CS/FlightTickets/MainWindow.xaml) (VB: [MainWindow.xaml](./VB/FlightTickets/MainWindow.xaml))
+* [MainWindow.xaml.cs](./CS/FlightTickets/MainWindow.xaml.cs) (VB: [MainWindow.xaml.vb](./VB/FlightTickets/MainWindow.xaml.vb))
+
+<!-- default file list end -->
